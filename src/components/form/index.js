c06441f106b8db86f31afe5100f3787591df72d6
Cullen Sharp <cullen.sharp100@gmail.com>
2021-07-20 16:49:38 -0700

    Render request url/method dynamically

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,32 +1,42 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState } from 'react';
 
 import './form.scss';
 
 function Form({ handleApiCall }) {
+  const [method, setMethod] = useState('');
+  const [url, setUrl] = useState('');
 
   const handleSubmit = e => {
     e.preventDefault();
     const formData = {
-      method:'GET',
-      url: 'https://pokeapi.co/api/v2/pokemon',
+      method,
+      url,
     };
     handleApiCall(formData);
   };
+
+  function getMethod(e){
+    setMethod(e.target.textContent);
+  }
+
+  function getUrl(e) {
+    setUrl(e.target.value);
+  }
 
   return (
     <>
       <form onSubmit={handleSubmit}>
         <label >
           <span>URL: </span>
-          <input name='url' type='text' />
-          <button type="submit">GO!</button>
+          <input name='url' type='text' value={url} onChange={getUrl}/>
+          <button type="submit" onClick={handleSubmit}>GO!</button>
         </label>
         <label className="methods">
-          <span id="get">GET</span>
-          <span id="post">POST</span>
-          <span id="put">PUT</span>
-          <span id="delete">DELETE</span>
+          <span id="get" onClick={getMethod}>GET</span> 
+          <span id="post" onClick={getMethod}>POST</span>
+          <span id="put" onClick={getMethod}>PUT</span>
+          <span id="delete" onClick={getMethod}>DELETE</span>
         </label>
       </form>
     </>
